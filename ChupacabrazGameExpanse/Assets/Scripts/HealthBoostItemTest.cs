b418de4e1897ed793032686d2889e0133e929c78
Rhustish <bbhatnagar27@gmail.com>
2023-08-28 20:29:09 +0530

    healthboost and sand pick up mechanic with their prefabs done

--- ChupacabrazGameExpanse/Assets/Scripts/HealthBoostItemTest.cs
+++ ChupacabrazGameExpanse/Assets/Scripts/HealthBoostItemTest.cs
@@ -1,20 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class SandItemTest : MonoBehaviour
+public class HealthBoostItemTest : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             playerTest pscript = collision.gameObject.GetComponent<playerTest>();
-            if (pscript != null && pscript.inventory.sand < 20)
+            if (pscript != null && pscript.inventory.health < 100)
             {
-                pscript.inventory.sand = pscript.inventory.sand + 1;
+                pscript.inventory.health = pscript.inventory.health + 1;
                 Destroy(gameObject);
             }
-            
         }
     }
 }
--- ChupacabrazGameExpanse/Assets/playerTest.cs
+++ ChupacabrazGameExpanse/Assets/playerTest.cs
@@ -1,10 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class playerTest : MonoBehaviour
 {
+    //Inventory management
+
+    public struct Item
+    {
+        public int id;
+        public GameObject thumbnail;
+        public string name;
+    }
+    public Item emptyShell;
+    public struct Inventory
+    {
+        public int health, sand;
+        public Item[] items;
+    }
+    public Inventory inventory;
+
+    //movement variables
     private Rigidbody2D rb;
     private float horizontal;
     private float playerSpeed;
@@ -16,11 +34,25 @@ public class playerTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //movement setup
         rb = gameObject.GetComponent<Rigidbody2D>();
         rayCastLength = 0.7f;
         isGrounded = true;
         playerSpeed = 5;
         jumpForce = 7;
+
+        //inventory init
+        emptyShell.id = 0;
+        emptyShell.thumbnail = null;
+        emptyShell.name = null;
+
+        inventory.health = 5;
+        inventory.sand = 0;
+        inventory.items = new Item[5];
+        for(int i = 0; i < inventory.items.Length; i++)
+        {
+            inventory.items[i] = emptyShell;
+        }
     }
 
     // Update is called once per frame
@@ -28,15 +60,15 @@ public class playerTest : MonoBehaviour
     {
         rb.velocity = new Vector2(x: horizontal * playerSpeed, y: rb.velocity.y);
         horizontal = Input.GetAxis("Horizontal");
-        //Debug.Log(horizontal);
 
+        //jump mechanic
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.velocity = new Vector2(x: rb.velocity.x, y: jumpForce);
         }
 
         isGrounded = Physics2D.Raycast(origin: transform.position, direction: Vector2.down, distance: rayCastLength, groundLayerMask);
-        Debug.DrawRay(transform.position, Vector3.down * rayCastLength, Color.red);
+
     }
 
 }
